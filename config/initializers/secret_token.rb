9618d4428a7e7c936ea0644d336d44ef655e460a
Alex <alexsiri7@gmail.com>
2013-06-03 07:02:17 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rentify2::Application.config.secret_token = '14f066e1df0e70f8b35326d680a232003923620f1308b13787a93671ecd02c3cddb16fc8dc75d7a17baf8e33def88583e5887a763b3659c9f61d8962c426cd12'
