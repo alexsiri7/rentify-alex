19c6ef535742e5a4997a96b8527734751f1aeb06
Alex <alexsiri7@gmail.com>
2013-06-03 22:44:11 +0100

    Prettify of Unit test

--- test/unit/rentify_test.rb
+++ test/unit/rentify_test.rb
@@ -23,8 +23,9 @@ class RentifyTest < ActiveSupport::TestCase
 
   def test_find_same_property
     @origin_property = Property.new({:name=>'Origin Property',:number_of_rooms=>NUMBER_OF_BEDROOMS, :latitude=>LAT, :longitude=>LONG})
-    expected_list = [@origin_property]
-    @rentify.setPropertyList(expected_list)
+    fixture_properties = [@origin_property]
+    expected_list      = [@origin_property]
+    @rentify.setPropertyList(fixture_properties)
 
     list = @rentify.run()
 
@@ -33,7 +34,7 @@ class RentifyTest < ActiveSupport::TestCase
 
   def test_order_by_distance
     fixture_properties = [@further_property,@close_property]
-    expected_list = [@close_property,@further_property]
+    expected_list      = [@close_property,@further_property]
     @rentify.setPropertyList(fixture_properties)
 
     list = @rentify.run()
@@ -43,7 +44,7 @@ class RentifyTest < ActiveSupport::TestCase
 
   def test_do_not_reorder_by_distance_if_already_ordered
     fixture_properties = [@close_property,@further_property]
-    expected_list = [@close_property,@further_property]
+    expected_list      = [@close_property,@further_property]
     @rentify.setPropertyList(fixture_properties)
 
     list = @rentify.run()
@@ -54,7 +55,7 @@ class RentifyTest < ActiveSupport::TestCase
   def test_filter_out_if_lesser_number_of_bedrooms
     @smaller_property = Property.new({:name=>'Smaller Property', :number_of_rooms=>LARGE_NUMBER_OF_BEDROOMS, :latitude=>LAT_LESS_1KM, :longitude=>LONG});
     fixture_properties = [@smaller_property]
-    expected_list = []
+    expected_list      = []
     @rentify.setPropertyList(fixture_properties)
 
     list = @rentify.run()
@@ -75,7 +76,7 @@ class RentifyTest < ActiveSupport::TestCase
   end
 
   def test_filter_by_distance
-    @far_property = Property.new({:name=>'Further Property',:number_of_rooms=>NUMBER_OF_BEDROOMS, :latitude=>LAT_OVER_20KM, :longitude=>LONG})
+    @far_property = Property.new({:name=>'Far Property',:number_of_rooms=>NUMBER_OF_BEDROOMS, :latitude=>LAT_OVER_20KM, :longitude=>LONG})
     fixture_properties = [@far_property]
     expected_list      = []
     @rentify.setPropertyList(fixture_properties)
